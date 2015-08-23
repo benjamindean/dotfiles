0fc37f2142c43e3a5e45bbfbd64227e6fbe5c571
Benjamin Dean <berman.reshetnikov@gmail.com>
2015-08-23 14:30:13 +0300

    Short install script added

---
+++ install.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+
+export DOTFILES_DIR
+DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+# Update dotfiles
+
+[ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
+
+# Create symlinks
+
+ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
+ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
