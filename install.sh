76666df4b8f08ba5871007b69c9404ecbeb70be6
Benjamin Dean <berman.reshetnikov@gmail.com>
2015-11-20 16:36:05 +0200

    .editorconfig added

--- install.sh
+++ install.sh
@@ -35,3 +35,4 @@ ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/.npmrc" ~
 ln -sfv "$DOTFILES_DIR/.aliases" ~
 ln -sfv "$DOTFILES_DIR/.functions" ~
+ln -sfv "$DOTFILES_DIR/.editorconfig" ~
