b069082ce7c63842b25fb1a30cc2a35da074e2df
Benjamin Dean <berman.reshetnikov@gmail.com>
2015-11-20 12:25:44 +0200

    Added Oh My Zsh installation

--- install.sh
+++ install.sh
@@ -1,7 +1,28 @@
 #!/usr/bin/env bash
 
 export DOTFILES_DIR
+export ZSHRC
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+ZSHRC="$HOME/.zshrc"
+
+# Install Oh My Zsh
+
+if [ ! -d "$HOME/.oh-my-zsh" ]; then
+
+    sudo apt-get install -y zsh
+    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
+    echo "\nsource ~/.aliases\nsource ~/.functions" >> ZSHRC
+
+    if [ git --version 2>/dev/null -eq 0 ]; then
+        sudo add-apt-repository ppa:git-core/ppa -y
+        sudo apt-get install git
+    else
+        git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
+        sed -i -- 's/plugins=(git)/plugins=(git zsh-syntax-highlighting)/g' ZSHRC
+        sed -i -- 's/ZSH_THEME="robbyrussell"/ZSH_THEME="gallois"/g' ZSHRC
+    fi
+
+fi
 
 # Update dotfiles
 
