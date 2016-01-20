160ff9e6fe12d2e6e1c1014fbed64608c51a06b9
Benjamin Dean <berman.reshetnikov@gmail.com>
2016-01-20 13:03:19 +0200

    Switch to Prezto

--- install.sh
+++ install.sh
@@ -3,37 +3,46 @@
 export DOTFILES_DIR
 export ZSHRC
 DOTFILES_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-ZSHRC="$HOME/.zshrc"
+ZSHRC="$HOME/.zpreztorc"
 
-# Install Oh My Zsh
+# Install git
 
-if [ ! -d "$HOME/.oh-my-zsh" ]; then
-
-    sudo apt-get install -y zsh
-    sh -c "$(wget https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
-    echo "\nsource ~/.aliases\nsource ~/.functions" >> ZSHRC
-    echo 'if [ -z "$SSH_AUTH_SOCK" ] ; then\n    eval `ssh-agent -s`\n    ssh-add\nfi' >> ZSHRC
-
-    if [ git --version 2>/dev/null -eq 0 ]; then
-        sudo add-apt-repository ppa:git-core/ppa -y
-        sudo apt-get install git
-    else
-        git clone git://github.com/zsh-users/zsh-syntax-highlighting.git $HOME/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
-        sed -i -- 's/plugins=(git)/plugins=(git zsh-syntax-highlighting)/g' ZSHRC
-        sed -i -- 's/ZSH_THEME="robbyrussell"/ZSH_THEME="gallois"/g' ZSHRC
-    fi
-
+if [ git --version 2>/dev/null -eq 0 ]; then
+    sudo add-apt-repository ppa:git-core/ppa -y
+    sudo apt-get update
+    sudo apt-get install git
 fi
 
 # Update dotfiles
 
 [ -d "$DOTFILES_DIR/.git" ] && git --work-tree="$DOTFILES_DIR" --git-dir="$DOTFILES_DIR/.git" pull origin master
 
+# Install Prezto
+
+if [ ! -d "$HOME/.zprezto" ]; then
+
+    sudo apt-get install -y zsh
+    sh -c "git clone --recursive https://github.com/sorin-ionescu/prezto.git '${ZDOTDIR:-$HOME}/.zprezto'"
+
+    setopt EXTENDED_GLOB
+    for rcfile in "${ZDOTDIR:-$HOME}/.zprezto/runcoms/^README.md(.N)"; do
+      ln -s "$rcfile" "${ZDOTDIR:-$HOME}/.${rcfile:t}"
+    done
+    ln -sf .zpreztorc $HOME/.zpreztorc
+    echo "\nsource ~/.aliases\nsource ~/.functions" >> ZSHRC
+
+    chsh -s /bin/zsh
+
+fi
+
 # Create symlinks
 
+ln -sfv "$DOTFILES_DIR/.zpreztorc" ~
 ln -sfv "$DOTFILES_DIR/git/.gitconfig" ~
 ln -sfv "$DOTFILES_DIR/git/.gitignore_global" ~
 ln -sfv "$DOTFILES_DIR/.npmrc" ~
 ln -sfv "$DOTFILES_DIR/.aliases" ~
 ln -sfv "$DOTFILES_DIR/.functions" ~
 ln -sfv "$DOTFILES_DIR/.editorconfig" ~
+
+echo "All done!"
