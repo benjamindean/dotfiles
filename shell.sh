5fb0a130c0f47e0f4011213c384a83b96fe01e7c
Benjamin Dean <berman.reshetnikov@gmail.com>
2016-10-12 13:53:37 +0200

    Install composer

--- shell.sh
+++ shell.sh
@@ -10,7 +10,8 @@ if [ ! -d "$HOME/.oh-my-zsh" ]; then
     source $HOME/.functions
     source /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh    
     export GOPATH=$HOME/.gocode
-    export GEM_HOME=~/.gem
+    export GEM_HOME=$HOME/.gem
+    export PATH=$GOPATH/bin:$HOME/.npm/bin:$HOME/.config/composer/vendor/bin:$GEM_HOME/bin:$PATH
     " >> ZSHRC
     chsh -s /bin/zsh
 fi
